08eb0c7855e13f456fa45b3be5b8d311159fafb3
Adam O'Neil <adamosoftware@gmail.com>
2024-01-03 08:26:17 -0500

    extracted API endpoints to new file

--- EmailLeadCapture.API/Program.cs
+++ EmailLeadCapture.API/Program.cs
@@ -1,9 +1,6 @@
 using EmailLeadCapture.API;
 using EmailLeadCapture.API.EFData;
-using EmailLeadCapture.Database;
 using HashidsNet;
-using Microsoft.EntityFrameworkCore;
-using EmailLead = EmailLeadCapture.Shared.EmailLead;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -30,75 +27,6 @@ app.UseAuthorization();
 
 app.MapGet("/", () => "AO Lead Capture API");
 
-app.MapPost("/confirm/{leadId}", async (string leadId, LeadCaptureDatabase database, Hashids hashIds) =>
-{
-	var emailLeadId = hashIds.DecodeSingle(leadId);
-	await database.DoTransactionAsync(async (cn, txn) =>
-	{
-		var emailLead = await database.EmailLeads.GetAsync(cn, emailLeadId, txn) ?? throw new Exception("Lead not found");
-		emailLead.IsConfirmed = true;
-		emailLead.ConfirmedDateUtc = DateTime.UtcNow;
-		await database.EmailLeads.SaveAsync(cn, emailLead, txn);
-	});	
-});
-
-app.MapPost("/optout/{leadId}", async (string leadId, LeadCaptureDatabase database, Hashids hashIds) => 
-	await SetOptStatus(leadId, database, hashIds, OptStatus.Out));
-
-app.MapPost("/optin/{leadId}", async (string leadId, LeadCaptureDatabase database, Hashids hashIds) => 
-	await SetOptStatus(leadId, database, hashIds, OptStatus.In));
-
-async Task SetOptStatus(string leadId, LeadCaptureDatabase database, Hashids hashIds, OptStatus optStatus)
-{
-	var emailLeadId = hashIds.DecodeSingle(leadId);
-	await database.DoTransactionAsync(async (cn, txn) =>
-	{
-		var emailLead = await database.EmailLeads.GetAsync(cn, emailLeadId, txn) ?? throw new Exception("Lead not found");
-		emailLead.OptStatus = optStatus;
-		emailLead.OptStatusChangedUtc = DateTime.UtcNow;
-		await database.EmailLeads.SaveAsync(emailLead);
-	});
-}
-
-var apiRoutes = app.MapGroup("/api").RequireAuthorization();
-
-apiRoutes.MapGet("/encode", (int number, Hashids hashIds) =>
-{
-	return new { value = hashIds.Encode(number) };
-});
-
-apiRoutes.MapGet("/applications", async (LeadCaptureDbContext db, Hashids hashIds) =>
-{
-	return await db.Applications.Select(row => new { row.Name, id = hashIds.Encode(row.Id) }).ToListAsync();
-});
-
-apiRoutes.MapPost("/save", async (Hashids hashIds, LeadCaptureDatabase database, EmailLead emailLead) =>
-{
-	var applicationId = hashIds.DecodeSingle(emailLead.ApplicationId);
-
-	var result = await database.EmailLeads.MergeAsync(new()
-	{
-		ApplicationId = applicationId,
-		Email = emailLead.Email
-	});
-
-	var app = await database.Applications.GetAsync(applicationId);
-
-	// todo: send email with link to confirmation page
-
-	return new { id = hashIds.Encode(result.Id) };
-});
-
-apiRoutes.MapGet("/{appId}/leads", async (string appId, LeadCaptureDbContext db, Hashids hashIds, int page = 0) =>
-{
-	const int pageSize = 50;
-	var applicationId = hashIds.DecodeSingle(appId);
-	return await db.EmailLeads
-		.Where(row => row.ApplicationId == applicationId && row.IsConfirmed && row.OptStatus == OptStatus.In)
-		.OrderBy(row => row.Email)
-		.Skip(page * pageSize).Take(pageSize)
-		.Select(row => new EmailLead() { Id = hashIds.Encode(row.Id), Email = row.Email })
-		.ToListAsync();
-});
+MapApiEndpoints(app);
 
 app.Run();
---
+++ EmailLeadCapture.API/Program_Api.cs
@@ -0,0 +1,83 @@
+﻿using EmailLeadCapture.API;
+using EmailLeadCapture.API.EFData;
+using EmailLeadCapture.Shared;
+using HashidsNet;
+using Microsoft.EntityFrameworkCore;
+
+internal partial class Program
+{	
+	static void MapApiEndpoints(WebApplication app)
+	{
+		app.MapPost("/confirm/{leadId}", async (string leadId, LeadCaptureDatabase database, Hashids hashIds) =>
+		{
+			var emailLeadId = hashIds.DecodeSingle(leadId);
+			await database.DoTransactionAsync(async (cn, txn) =>
+			{
+				var emailLead = await database.EmailLeads.GetAsync(cn, emailLeadId, txn) ?? throw new Exception("Lead not found");
+				emailLead.IsConfirmed = true;
+				emailLead.ConfirmedUtc = DateTime.UtcNow;
+				await database.EmailLeads.SaveAsync(cn, emailLead, txn);
+			});
+		});
+
+		app.MapPost("/optout/{leadId}", async (string leadId, LeadCaptureDatabase database, Hashids hashIds) =>
+			await SetOptStatus(leadId, database, hashIds, false));
+
+		app.MapPost("/optin/{leadId}", async (string leadId, LeadCaptureDatabase database, Hashids hashIds) =>
+			await SetOptStatus(leadId, database, hashIds, true));
+
+		async Task SetOptStatus(string leadId, LeadCaptureDatabase database, Hashids hashIds, bool optIn)
+		{
+			var emailLeadId = hashIds.DecodeSingle(leadId);
+			await database.DoTransactionAsync(async (cn, txn) =>
+			{
+				var emailLead = await database.EmailLeads.GetAsync(cn, emailLeadId, txn) ?? throw new Exception("Lead not found");
+				emailLead.IsOptedIn = optIn;
+				emailLead.OptChangedUtc = DateTime.UtcNow;
+				await database.EmailLeads.SaveAsync(emailLead);
+			});
+		}
+
+		var apiRoutes = app.MapGroup("/api").RequireAuthorization();
+
+		apiRoutes.MapGet("/encode", (int number, Hashids hashIds) =>
+		{
+			return new { value = hashIds.Encode(number) };
+		});
+
+		apiRoutes.MapGet("/applications", async (LeadCaptureDbContext db, Hashids hashIds) =>
+		{
+			return await db.Applications.Select(row => new { row.Name, id = hashIds.Encode(row.Id) }).ToListAsync();
+		});
+
+		apiRoutes.MapPost("/save", async (Hashids hashIds, LeadCaptureDatabase database, EmailLead emailLead) =>
+		{
+			var applicationId = hashIds.DecodeSingle(emailLead.ApplicationId);
+
+			var result = await database.EmailLeads.MergeAsync(new()
+			{
+				ApplicationId = applicationId,
+				Email = emailLead.Email
+			});
+
+			var app = await database.Applications.GetAsync(applicationId);
+
+			// todo: send email with link to confirmation page
+
+			return new { id = hashIds.Encode(result.Id) };
+		});
+
+		apiRoutes.MapGet("/{appId}/leads", async (string appId, LeadCaptureDbContext db, Hashids hashIds, int page = 0) =>
+		{
+			const int pageSize = 50;
+			var applicationId = hashIds.DecodeSingle(appId);
+			return await db.EmailLeads
+				.Where(row => row.ApplicationId == applicationId && row.IsConfirmed && row.IsOptedIn)
+				.OrderBy(row => row.Email)
+				.Skip(page * pageSize).Take(pageSize)
+				.Select(row => new EmailLead() { Id = hashIds.Encode(row.Id), Email = row.Email })
+				.ToListAsync();
+		});
+
+	}
+}
